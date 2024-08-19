a01abfdd7c4ddaa96e52d4386c1a786a5699d573
chulecita <jugutierrez@unc.edu.ar>
2024-08-18 23:54:06 -0300

    ajusto calendar

--- Front/js/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const dropdownToggle = document.querySelector('.dropdown-toggle');
-    const dropdownMenu = document.querySelector('.dropdown-menu');
-
-    dropdownToggle.addEventListener('click', (e) => {
-        e.preventDefault();
-        dropdownMenu.classList.toggle('show');
-    });
-});
