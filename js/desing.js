c2efc820c15075eea13f12d8aec342fe2da28167
Belen Taborda <mbelen.taborda41@gmail.com>
2024-09-05 10:53:30 -0300

    Revert "parallax working"
    This reverts commit 409db2c5d845819a62f37720f201fe25c99bbc3a.

--- js/desing.js
+++ js/desing.js
@@ -1,4 +1,5 @@
-
+let text = document.getElementById('text');
+let leaf = document.getElementById('leaf');
 let ilust2 = document.getElementById('ilust2');
 let ilust3 = document.getElementById('ilust3');
 let ilust4 = document.getElementById('ilust4');
@@ -6,11 +7,11 @@ let ilust4 = document.getElementById('ilust4');
 window.addEventListener('scroll',() => {
     let value = window.scrollY;
 
-    ilust4.style.top = value * 2.5 + 'px';
-    ilust2.style.top = value * -1.5 + 'px';
+    text.style.margingTop = value * 2.5 + 'px';
+    leaf.style.Top = value * -1.5 + 'px';
+    leaf.style.left = value * 1.5 + 'px';
     ilust2.style.left = value * 1.5 + 'px';
-    ilust3.style.top = value * -1.5 + 'px';
-    ilust3.style.left = value * -1.5 + 'px';
-    
+    ilust3.style.right = value * -1.5 + 'px';
+    ilust4.style.right = value * 1 + 'px';
 
 }) ;
